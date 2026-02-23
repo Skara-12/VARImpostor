b3a98d307c6aaf89d7d0a3bb56809bae59711fb5
Rali <rali.skalli98@gmail.com>
2026-02-23 15:53:12 -0500

    Initial Commit

---
+++ VARImpostor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VARImpostor
+//
+//  Created by Rali Skalli on 23/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VARImpostor/VARImpostorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VARImpostorApp.swift
+//  VARImpostor
+//
+//  Created by Rali Skalli on 23/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct VARImpostorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
